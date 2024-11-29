156a4650e993b4501e3c2d9b58c99d76f11dbb69
Ashan Kasthuriarachchi <145893442+ashan2001ruh@users.noreply.github.com>
2024-11-30 00:23:54 +0530

    Updated navbar component to fix responsive behavior and menu toggle

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,26 +1,43 @@
 import React from 'react'
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 
-const navbar = () => {
+const Navbar = () => {
+  const [nav, setNav] = React.useState(false)
+
+  const handleNav = () => {
+    setNav(!nav)
+  }
+
   return (
     <div className="flex justify-between items-center h-20 max-w-[1240px] mx-auto px-8 text-[#004581]">
       {/* Logo */}
       <h1 className="text-2xl md:text-3xl flex space-x-6 md:space-x-8 font=sans font-bold text-[#004581]">MCQuiz</h1>
       <div className="flex items-center space-x-6 md:space-x-8">
       {/* Navigation Menu */}
-      <ul className="flex space-x-6 md:space-x-8 font-semibold text-lg md:text-base">
+      <ul className="hidden md:flex space-x-6 md:space-x-8 font-semibold text-lg md:text-base">
         <li className="hover:text-[#018ABD] cursor-pointer">Home</li>
         <li className="hover:text-[#018ABD] cursor-pointer">Features</li>
         <li className="hover:text-[#018ABD] cursor-pointer">Pricing</li>
         <li className="hover:text-[#018ABD] cursor-pointer">About Us</li>
       </ul>
-
-      {/* Login Button */}
-      <button className="hidden md:block bg-[#018ABD] text-white font-semibold px-6 py-2 rounded-2xl text-sm hover:bg-[#005fa3] transition duration-200">
-        LOG IN
-      </button>
+      <div onClick={handleNav} className='block md:hidden'>
+          {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
       </div>
-    </div>
-  )
-}
+      <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-blue-900 bg-[#DDE8F0]/50 backdrop-blur-lg ease-in-out duration-500' : 'fixed left-[-100%]'}>
+      <h1 className="w-full text-3xl font-bold text-[#004581] m-4">MCQuiz</h1>
 
-export default navbar
+        <ul className=' font-semibold p-4'>
+          <li className='p-4 border-b border-blue-900 '>Home</li>
+          <li className='p-4 border-b border-blue-900 '>Features</li>
+          <li className='p-4 border-b border-blue-900 '>Pricing</li>
+          <li className='p-4'>About Us</li>
+        </ul>
+      </div>
+
+      </div>
+      
+    </div>
+  );
+};
+
+export default Navbar
