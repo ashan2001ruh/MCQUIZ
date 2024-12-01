06917f1655647ddb9e639f56239c83370696998b
Ashan Kasthuriarachchi <145893442+ashan2001ruh@users.noreply.github.com>
2024-12-01 16:40:45 +0530

    created the hero section

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 
 const Navbar = () => {
-  const [nav, setNav] = React.useState(false)
+  const [nav, setNav] = useState(false);
 
   const handleNav = () => {
-    setNav(!nav)
-  }
+    setNav(!nav);
+  };
 
   return (
     <div className="flex justify-between items-center h-20 max-w-[1240px] mx-auto px-8 text-[#004581]">
@@ -20,15 +20,19 @@ const Navbar = () => {
         <li className="hover:text-[#018ABD] cursor-pointer">Pricing</li>
         <li className="hover:text-[#018ABD] cursor-pointer">About Us</li>
       </ul>
-      <div onClick={handleNav} className='block md:hidden cursor-pointer z-50'>
-          {!nav ? <AiOutlineMenu size={20}/> : <AiOutlineClose size={20} />}
+
+      {/* Login Button */}
+      <button className="hidden md:block bg-[#018ABD] text-white font-semibold px-6 py-2 rounded-2xl text-sm hover:bg-[#005fa3] transition duration-200">
+        LOG IN
+      </button>
+      <div onClick={handleNav} className='block md:hidden'>
+          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
       </div>
-      <div className={`fixed left-0 top-0 w-[60%] h-full border-r border-blue-900 bg-[#DDE8F0]/50 backdrop-blur-lg ease-in-out duration-500 transform ${
-          nav ? 'left-0' : '-left-[100%]'
-        }`}>
+      <ul className={nav ? 'fixed left-0 top-0 w-[50%] h-full border-r  border-blue-900 bg-[#DDE8F0]/50 backdrop-blur-lg ease-in-out duration-500 ' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+        
       <h1 className="w-full text-3xl font-bold text-[#004581] m-4">MCQuiz</h1>
 
-        <ul className=' font-semibold p-4'>
+        
           <li className='p-4 border-b border-blue-900 '>Home</li>
           <li className='p-4 border-b border-blue-900 '>Features</li>
           <li className='p-4 border-b border-blue-900 '>Pricing</li>
@@ -36,7 +40,7 @@ const Navbar = () => {
         </ul>
       </div>
 
-      </div>
+
       
     </div>
   );
